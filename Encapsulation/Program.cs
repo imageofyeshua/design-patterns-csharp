4d7c59ea481b88209e6e88a07d441cbb11fda85e
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-04 08:53:44 +0900

    Encapsulation

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿BankAccount bankAccount = new BankAccount(100);
+
+Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Deposit(50);
+
+Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Withdraw(20);
+
+Console.WriteLine(bankAccount.GetBalance());
